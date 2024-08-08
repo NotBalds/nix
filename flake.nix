5a0f83851e1e22cb44c2a93772b5f737c5af1fe0
ProggerX <x@proggers.ru>
2024-08-08 22:35:23 +0300

    global: initial commit
    packages added:
    bald/1.0.0
    bal/2.0.0
    cwe-client-cli/0.2.0
    cwe-server/0.2.0

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+	description = "NotBalds flake - place of NotBalds' nix things.";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs";
+		cwe-server.url = "github:NotBalds/cwe-server/v0.2.0";
+		cwe-cli.url = "github:NotBalds/cwe-client-cli/0.2.0";
+		bald.url = "github:NotBalds/bald";
+		bal.url = "github:NotBalds/bal";
+	};
+
+	outputs = { nixpkgs, ... }@inputs: {
+		packages = (nixpkgs.lib.forEach ["aarch64-linux" "x86_64-linux"] (system: {
+			cwe-server = inputs.cwe-server.packages.${system}.default;
+			cwe-client-cli = inputs.cwe-cli.packages.${system}.default;
+			bald = inputs.bald.packages.${system}.default;
+			bal = inputs.bal.packages.${system}.default;
+		}));
+	};
+}
